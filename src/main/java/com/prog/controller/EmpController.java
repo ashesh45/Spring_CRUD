88c48f934e9846b3a6f0ab5ffe5c8dbf6122a8f3
Ashes Maharjan <ashes.maharjan@infodevelopers.local>
2023-06-13 15:28:24 +0545

    emp version 2

--- src/main/java/com/prog/controller/EmpController.java
+++ src/main/java/com/prog/controller/EmpController.java
@@ -1,14 +1,22 @@
 package com.prog.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.prog.entity.Employee;
+import com.prog.service.EmpService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class EmpController {
+	
+    @Autowired
+	private EmpService service;
 	
 	@GetMapping("/")
 	public String home()
@@ -25,8 +33,8 @@ public class EmpController {
 	@PostMapping("/register")
 	public String empRegister(@ModelAttribute Employee e)
 	{
-		System.out.println(e);
-		return "addemp";
+		service.addEmp(e);
+		return "redirect:/";
 	}
 	
 }
