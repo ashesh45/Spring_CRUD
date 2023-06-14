4ab1e2c62ca3cd72b1d9ba583654badc8b060f1d
Ashes Maharjan <ashes.maharjan@infodevelopers.local>
2023-06-14 07:53:17 +0545

    CRUD

--- src/main/java/com/prog/service/EmpService.java
+++ src/main/java/com/prog/service/EmpService.java
@@ -1,4 +1,6 @@
 package com.prog.service;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +17,27 @@ public class EmpService {
 	{
 		repo.save(e);
 	}
+	
+	public List<Employee> getAllEmp()
+	{
+		return repo.findAll();
+	}
+	
+	public Employee getEmpById(int id)
+	
+	{
+		Optional<Employee> e=repo.findById(id);
+		if(e.isPresent())
+		{
+			return e.get();
+		}
+		return null;
+	}
+	
+	public void deleteEmp(int id)
+	{
+		repo.deleteById(id);
+	}
+	
 
 }
